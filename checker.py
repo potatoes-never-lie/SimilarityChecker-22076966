4263cb96889511402c54d2e42e12dd9f65019f6d
chaeyeon.rhee <chaeyeon.rhee@gmail.com>
2026-07-01 16:54:25 +0900

    [Feature] get alpha similarity

--- checker.py
+++ checker.py
@@ -3,6 +3,7 @@ import math
 class SimilarityChecker:
 
     LENGTH_MULTIPLIER = 60
+    ALPHA_MULTIPLIER = 40
 
     def __init__(self, word1:str, word2:str):
         self.word1 : str = word1
@@ -16,3 +17,16 @@ class SimilarityChecker:
         _short_length = min(_word1_length, _word2_length)
 
         return round((1 - (_gap / _short_length)) * self.LENGTH_MULTIPLIER, 3)
+
+    @property
+    def same_letters_cnt(self) -> int:
+        same_letters = set(self.word1) & set(self.word2)
+        return len(same_letters)
+
+    @property
+    def total_letters_cnt(self) -> int:
+        total_letters = set(self.word1) | set(self.word2)
+        return len(total_letters)
+
+    def get_alpha_similarity(self):
+        return round(self.same_letters_cnt / self.total_letters_cnt * self.ALPHA_MULTIPLIER, 3)
--- test_checker.py
+++ test_checker.py
@@ -1,6 +1,5 @@
 import pytest
 
-import checker
 from checker import SimilarityChecker
 
 
@@ -14,3 +13,14 @@ def test_length(word1, word2, len_score):
 
     c = SimilarityChecker(word1, word2)
     assert c.get_length_similarity() == len_score
+
+@pytest.mark.parametrize("word1, word2, alpha_score", [
+    ("ASD", "DSA", 40),
+    ("A", "BB", 0),
+    ("AAABB", "BAA", 40),
+    ("AA", "AAE", 20)
+])
+def test_alpha(word1, word2, alpha_score):
+
+    c = SimilarityChecker(word1, word2)
+    assert c.get_alpha_similarity() == alpha_score
